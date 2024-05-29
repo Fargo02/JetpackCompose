df7d66761be5894f4a4b53f67ec290b5640224bb
Nikita <trenin.0235@gmail.com>
2024-05-29 15:11:14 +0700

    API

--- app/src/main/java/com/example/jetpackcompose/ServerApi.kt
+++ app/src/main/java/com/example/jetpackcompose/ServerApi.kt
@@ -6,18 +6,17 @@ import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 
 interface ServerApi {
-    companion object {
-        fun create(): ServerApi {
-            return Retrofit.Builder()
-                .baseUrl("https://github.com/Fargo02/JetpackCompose/blob/master/app/src/main/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
-                .create(ServerApi::class.java)
-        }
-
-    }
-    @GET("/java/com/example/jetpackcompose/data/location.json")
-    fun getCatalog(): Call<CategoryResponse>
+//    companion object {
+//        fun create(): ServerApi {
+//            return Retrofit.Builder()
+//                .baseUrl("https://raw.githubusercontent.com/Fargo02/JetpackCompose/")
+//                .addConverterFactory(GsonConverterFactory.create())
+//                .build()
+//                .create(ServerApi::class.java)
+//        }
+//    }
+    @GET("/master/app/src/main/java/com/example/jetpackcompose/data/location.json")
+    fun getCategory(): Call<CategoryResponse>
 }
 
 data class CategoryResponse (
